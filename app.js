7fe0de9af48a5170e21225ab6e7309ec692637bf
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-02-24 17:46:54 -0600

    created class using ES6 syntax

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-function Person(name, age, city) {
-    this.name = name;
-    this.age = age;
-    this.city = city;
+class Person {
+    constructor(name, age, city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+    sayHello() {
+        console.log(`Hey! My name is ${this.name}. I am ${this.age} years old and live in ${this.city}.`);
+    }
 }
 
-Person.prototype.sayHello = function () {
-    console.log(`Hey! My name is ${this.name}. I am ${this.age} years old and live in ${this.city}.`);
-}
 
 let p1 = new Person('Bradford', 29, 'Atlanta');
 let p2 = new Person('Irving', 29, 'Birmingham');
@@ -14,8 +16,6 @@ let p3 = new Person('Jack', 29, 'Atlanta');
 let p4 = new Person('John', 29, 'Atlanta');
 let p5 = new Person('Andy', 29, 'Atlanta');
 let p6 = new Person('Thomas', 29, 'Atlanta');
-
-
 
 p1.sayHello();
 p2.sayHello();
